a158717c23b1d2af5a7eb0587aa126141871f7e8
janisbahensons <markozzy333@gmail.com>
2023-03-08 15:49:56 +0200

    added test case

--- tests/grid.test.ts
+++ tests/grid.test.ts
@@ -39,5 +39,19 @@ describe("Testing Grid", () => {
         grid.createTile({x: 2, y:0, z:-2, value: 4})
         expect(grid.tiles.length).toEqual(0)
     })
+
+    it('Should not be able to add tile when hexagon is already taken', () => {
+      const grid = new Grid(1);
+      grid.createTile({x: 0, y:0, z:0, value: 4})
+      grid.createTile({x: 0, y:0, z:0, value: 2})
+      expect(grid.tiles).toContainEqual({
+          currPos: {x: 0, y: 0, z: 0} as Cordinates,
+          previousPos: null,
+          mergedFrom: null,
+          value: 4
+      } as Tile)
+
+      expect(grid.tiles.length).toEqual(1)
+  })
   })
 });
